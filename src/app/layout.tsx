e9ce9ec77f9c36d23595ee748788d2a5d2b9a7b5
Tobiloba Ibraheem <ibraheemtobiloba15@gmail.com>
2026-01-24 15:00:50 +0100

    worked on the auth...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,14 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner"
 
-const unboundedSans = localFont({
+const unbounded = localFont({
   src: [
-    { path: "../../public/fonts/Unbounded/Unbounded-Regular.ttf", weight: "400", style: "normal" },
-    { path: "../../public/fonts/Unbounded/Unbounded-Medium.ttf", weight: "500", style: "normal" },
-    { path: "../../public/fonts/Unbounded/Unbounded-SemiBold.ttf", weight: "600", style: "normal" },
-    { path: "../../public/fonts/Unbounded/Unbounded-Bold.ttf", weight: "700", style: "normal" },
-    { path: "../../public/fonts/Unbounded/Unbounded-Black.ttf", weight: "900", style: "normal" },
-    { path: "../../public/fonts/Unbounded/Unbounded-ExtraBold.ttf", weight: "800", style: "normal" },
+    { path: "./fonts/Unbounded/Unbounded-Regular.ttf", weight: "400", style: "normal" },
+    { path: "./fonts/Unbounded/Unbounded-Medium.ttf", weight: "500", style: "normal" },
+    { path: "./fonts/Unbounded/Unbounded-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "./fonts/Unbounded/Unbounded-Bold.ttf", weight: "700", style: "normal" },
+    { path: "./fonts/Unbounded/Unbounded-Black.ttf", weight: "900", style: "normal" },
+    { path: "./fonts/Unbounded/Unbounded-ExtraBold.ttf", weight: "800", style: "normal" },
   ],
   variable: "--font-unbounded-sans",
   display: "swap",
@@ -18,13 +18,13 @@ const unboundedSans = localFont({
 
 const unageo = localFont({
   src: [
-    { path: "../../public/fonts/Unageo/Unageo-Light.ttf", weight: "300", style: "normal" },
-    { path: "../../public/fonts/Unageo/Unageo-Regular.ttf", weight: "400", style: "normal" },
-    { path: "../../public/fonts/Unageo/Unageo-Medium.ttf", weight: "500", style: "normal" },
-    { path: "../../public/fonts/Unageo/Unageo-Semibold.ttf", weight: "600", style: "normal" },
-    { path: "../../public/fonts/Unageo/Unageo-Bold.ttf", weight: "700", style: "normal" },
-    { path: "../../public/fonts/Unageo/Unageo-Extrabold.ttf", weight: "800", style: "normal" },
-    { path: "../../public/fonts/Unageo/Unageo-Black.ttf", weight: "900", style: "normal" },
+    { path: "./fonts/Unageo/Unageo-Light.ttf", weight: "300", style: "normal" },
+    { path: "./fonts/Unageo/Unageo-Regular.ttf", weight: "400", style: "normal" },
+    { path: "./fonts/Unageo/Unageo-Medium.ttf", weight: "500", style: "normal" },
+    { path: "./fonts/Unageo/Unageo-Semibold.ttf", weight: "600", style: "normal" },
+    { path: "./fonts/Unageo/Unageo-Bold.ttf", weight: "700", style: "normal" },
+    { path: "./fonts/Unageo/Unageo-Extrabold.ttf", weight: "800", style: "normal" },
+    { path: "./fonts/Unageo/Unageo-Black.ttf", weight: "900", style: "normal" },
   ],
   variable: "--font-unageo-sans",
   display: "swap",
@@ -42,7 +42,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${unboundedSans.variable} ${unageo.variable} antialiased`}>
+      <body className={`${unbounded.variable} ${unageo.variable} antialiased`}>
         <Toaster />
         {children}
       </body>
